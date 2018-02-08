67748474269aab01bffbcb75784062555d6d5a23
mohammad karimi <m35_karimi@yahoo.com>
2018-02-08 11:39:40 +1000

    Add the authentication function using the username and password that has been recieved from client

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,65 @@
 const expres = require('express');
-const router  = expres.Router();
+const router = expres.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
 const User = require('../accessDb/user');
-
+const config = require('../config/database');
 
 //Register
-router.post('/register' ,(req,res ,next)=>{
-    User.addUser(req.body,(err,result)=>{
-        if(err){
-            res.json({success : false , msg : 'failed to registser user'});
-        }else{
-            res.json({success : true , msg : 'user registerd successfully'});
+router.post('/register', (req, res, next) => {
+    User.addUser(req.body, (err, result) => {
+        if (err) {
+            res.json({success: false, msg: 'failed to registser user'});
+        } else {
+            res.json({success: true, msg: 'user registerd successfully'});
         }
     })
 });
 
 //Authenticate
-router.post('/authenticate' ,(req,res,next)=>{
-    res.send('Authenticated');
-} )
+router.post('/authenticate', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.getUserByUsername(username, (err, user) => {
+        if (err) 
+            res.json({success: false, msg: err});
+        if (!user) 
+            res.json({success: false, msg: 'user not exist'});
+        else {
+            User.comparePassword(password, user.password, (err, isMatch) => {
+                if (err) 
+                    res.json({success: false, msg: err});
+                if (isMatch) {
+                    const token = 
+                    jwt.sign(
+                        {email:user.email,
+                        username : user.username,
+                        name : user,
+                        _id : user._id}, 'mysecret', {
+                        expiresIn: 200 //this is in second
+                    });
+                    res.json({success: true,
+                         token: 'JWT '+ token,
+                          user : {
+                              id : user._id,
+                              name : user.name,
+                              email : user.email,
+                              username : user.username
+                          }
+                        });
+                } else 
+                    res.json({success: false, msg: 'Password is incorrect'})
+            });
+        }
+
+    })
+})
 
 //Profile
-router.get('/profile' , (req,res,next)=>{
+router.get('/profile', (req, res, next) => {
     res.send('profile');
 })
 
-
-module.exports =router;
+module.exports = router;

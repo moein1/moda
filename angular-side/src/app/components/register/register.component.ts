faff94f482e0ec3a2f422b3b94d165ff0a38f0c2
mohammad karimi <m35_karimi@yahoo.com>
2018-02-11 05:29:36 +1000

    Add the auth service to th register component and try to subscrible to the response comming from the server and if the login is successful redirect to the login page.

--- angular-side/src/app/components/register/register.component.ts
+++ angular-side/src/app/components/register/register.component.ts
@@ -1,40 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { ValidateService } from 'app/services/validate.service';
+import {Component, OnInit} from '@angular/core';
+import {ValidateService} from 'app/services/validate.service';
 import {FlashMessagesService} from 'angular2-flash-messages';
+import { AuthService } from 'app/services/auth.service';
+import {Router} from '@angular/router';
 
-
-@Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
-})
+@Component({selector: 'app-register', templateUrl: './register.component.html', styleUrls: ['./register.component.css']})
 export class RegisterComponent implements OnInit {
   name : string;
   username : string;
   email : string;
   password : string;
 
-  constructor(private validateService : ValidateService,
-  private flashMessageService :FlashMessagesService) { }
+  constructor(private validateService : ValidateService, 
+    private flashMessageService : FlashMessagesService,
+    private authService : AuthService,
+    private router : Router ) {}
 
-  onRegister=()=>{
+  onRegister = () => {
     const user = {
-      name : this.name,
-      username : this.username,
-      email : this.email,
-      password : this.password
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
     }
-    if(!this.validateService.validateRegister(user) ){
-      this.flashMessageService.show('Please fill the fields' ,
-      {timeout : 2000 , cssClass:'invalid' }  )
+    if (!this.validateService.validateRegister(user)) {
+      this
+        .flashMessageService
+        .show('Please fill the fields', {
+          timeout: 2000,
+          cssClass: 'invalid'
+        });
+        return false;
     }
-    if(!this.validateService.validateEmail(this.email)){
-      this.flashMessageService.show('Email is not valid',
-      {timeout : 2000 , cssClass : 'invalid'})
+    if (!this.validateService.validateEmail(this.email)) {
+      this
+        .flashMessageService
+        .show('Email is not valid', {
+          timeout: 2000,
+          cssClass: 'invalid'
+        });
+        return false;
     }
+
+    this.authService.registerUser(user).subscribe(data=>{
+      console.log(data);
+      if(data.success){
+        this.flashMessageService.show('You are now registered and can login',{
+          timeout : 2000,
+          cssClass : 'invalid'
+        })
+        this.router.navigate(['/login']);
+      }else{
+        this.flashMessageService.show('Somethings went wrong ',
+      {
+        timeout : 2000,
+        cssClass : 'invalid'
+      });
+      this.router.navigate(['/register']);
+      }
+    })
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }

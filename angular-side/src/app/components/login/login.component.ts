4d01428aa142f416527a8411aa91075ad5f904dd
mohammad karimi <m35_karimi@yahoo.com>
2018-02-11 09:27:35 +1000

    Add the login page content for authentication

--- angular-side/src/app/components/login/login.component.ts
+++ angular-side/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from 'app/services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages/module/flash-messages.service';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username :string;
+  password : string;
+  showSpinner : boolean;
+  constructor(private validateSerive : ValidateService ,
+              private flashMessageService : FlashMessagesService) { }
 
-  constructor() { }
+  onLogin=()=>{
+    const auth = {username : this.username , password : this.password};
+    if(!this.validateSerive.validateInput(auth)){
+      this.flashMessageService.show('Please fills the input' ,{
+        timeout : 2000,
+        cssClass : 'alert-danger'
+      })
+    }else{
+      this.showSpinner = true;
+
+    }
+  }
 
   ngOnInit() {
   }

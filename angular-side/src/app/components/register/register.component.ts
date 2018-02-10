913ae5dfd75bb5a0b8272acd6ccc7da832428f67
mohammad karimi <m35_karimi@yahoo.com>
2018-02-11 04:50:26 +1000

    Add the validation and new angular flash message to the register module

--- angular-side/src/app/components/register/register.component.ts
+++ angular-side/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from 'app/services/validate.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
+
 
 @Component({
   selector: 'app-register',
@@ -7,14 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   name : string;
-  fullname : string;
+  username : string;
   email : string;
   password : string;
 
-  constructor() { }
+  constructor(private validateService : ValidateService,
+  private flashMessageService :FlashMessagesService) { }
 
   onRegister=()=>{
-    console.log(this.name);
+    const user = {
+      name : this.name,
+      username : this.username,
+      email : this.email,
+      password : this.password
+    }
+    if(!this.validateService.validateRegister(user) ){
+      this.flashMessageService.show('Please fill the fields' ,
+      {timeout : 2000 , cssClass:'invalid' }  )
+    }
+    if(!this.validateService.validateEmail(this.email)){
+      this.flashMessageService.show('Email is not valid',
+      {timeout : 2000 , cssClass : 'invalid'})
+    }
   }
 
   ngOnInit() {

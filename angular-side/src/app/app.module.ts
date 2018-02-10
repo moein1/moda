7d6150d8e7b04917bb6e6122912fa4ce61e025dd
mohammad karimi <m35_karimi@yahoo.com>
2018-02-11 04:48:37 +1000

    Add a new module name angular-flash-message that we can use for show the message

--- angular-side/src/app/app.module.ts
+++ angular-side/src/app/app.module.ts
@@ -11,6 +11,10 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ValidateService } from 'app/services/validate.service';
+import {FlashMessagesModule} from 'angular2-flash-messages';
+import { AuthService } from 'app/services/auth.service';
+
 
 //routing
 const appRoutes : Routes=[
@@ -35,9 +39,10 @@ const appRoutes : Routes=[
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [ValidateService,AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
